457573e06280e84ab449eb756648753cc557d77c
mfrabott <mfrabott14@gmail.com>
2023-01-08 14:21:04 -0500

    add project images to portfolio page

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -5,9 +5,9 @@ export default function Resume() {
     <section className='resume'>
       <h2 className='section-heading'>Resume</h2>
       <p>Download <span className='pdf'>PDF</span></p>
-      <p> Languages: JavaScript ES6+, CSS3, HTML5, SQL, NoSQL </p>
-      <p>Applications: GitHub, MongoDB, MySQL</p>
-      <p>Tools: React, Express, Node, Handlebars, Bootstrap</p>   
+      <p><span className='topic'>Languages </span> | JavaScript ES6+, CSS3, HTML5, SQL, NoSQL </p>
+      <p><span className='topic'>Applications </span> | GitHub, MongoDB, MySQL</p>
+      <p><span className='topic'>Tools </span> | React, Express, Node, Handlebars, Bootstrap</p>   
     </section>
   );
 }
