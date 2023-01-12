45b58ec83254d0f1167180afde171bf2e1c392c7
mfrabott <mfrabott14@gmail.com>
2023-01-11 21:14:39 -0500

    fix link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,7 +13,7 @@ export default function Header() {
           <a className='emailLogo' href="https://www.github.com/mfrabott" target="_blank" ><img src={emailLogo} className="contact-link contact-link-img"></img></a>
           <a href="https://www.github.com/mfrabott" target="_blank" ><img src={ghLogo} className="contact-link contact-link-img"></img></a>
           <a href="https://www.linkedin.com/in/michael-frabott" target="_blank"><img src={liLogo} className="contact-link contact-link-img"></img></a>
-          <a className='phoneLogo' href="https://www.github.com/mfrabott" target="_blank" ><img src={phoneLogo} className="contact-link contact-link-img"></img></a>
+          <a className='phoneLogo' href="tel:6143063071" target="_blank" ><img src={phoneLogo} className="contact-link contact-link-img"></img></a>
           <p className='contact-spelled-out'><span className="contact-link"><a href="tel:6143063071">Dial</a></span> | (614) 306-3071</p>
       </section>
     </div>
